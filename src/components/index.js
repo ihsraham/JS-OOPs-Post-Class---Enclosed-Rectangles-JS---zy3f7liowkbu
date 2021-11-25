89d20962154701ecb8f9b7ec336d80b7d6f8c25e
Maharshi Mishra <ihsraham27@gmail.com>
2021-11-25 18:01:21 +0530

    Made changes according to the question

--- src/components/index.js
+++ src/components/index.js
@@ -6,8 +6,22 @@
 //      children: []
 //}
 
-function updateStructure(rec1,rec2){
-	//write your code
+function updateStructure(rec1, rec2) {
+  //write your code
+  let rec = {};
+  rec.top = Math.min(rec1.top, rec2.top);
+  rec.left = Math.min(rec1.left, rec2.left);
+  rec.height = Math.max(rec1.height, rec2.height);
+  rec.width = Math.max(rec1.width, rec2.width);
+  rec.children = [];
+  let childObj = {};
+  childObj.top = Math.max(rec1.top, rec2.top) - Math.min(rec1.top, rec2.top);
+  childObj.left =
+    Math.max(rec1.left, rec2.left) - Math.min(rec1.left, rec2.left);
+  childObj.height = Math.min(rec1.height, rec2.height);
+  childObj.width = Math.min(rec1.width, rec2.width);
+  rec.children.push(childObj);
+  return rec;
 }
 
 module.exports = updateStructure;
